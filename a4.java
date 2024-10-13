2a9cb1286e3270f24f3f5a3a6812db335c47ca36
Akashiyappan <akashiyappan170@gmail.com>
2024-10-13 12:57:31 +0530

    Add files via upload

---
+++ a4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        System.out.println(integer.toBinarystring(a));
+        
+    }
+}
